07123b93aa85739b14ec6659ebb319cf95f09d1d
Hooch <aaron@aaron-Lenovo-B560.(none)>
2013-02-25 21:19:43 -0600

    Fully converted static HTML to Jinja templates.

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/resume')
+def resume():
+    return flask.render_template('resume.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)

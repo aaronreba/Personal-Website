87a4c4110dffdcf9ee4cad9c7ec04d718856e8a7
Aaron <aaronreba@gmail.com>
2018-04-20 22:51:48 -0500

    Make site more interesting

--- app.py
+++ app.py
@@ -2,21 +2,36 @@ import sys
 sys.path.append('./lib/python2.7/site-packages/')
 
 import os
+import platform
 import flask
+
+import datetime
 
 app = flask.Flask(__name__)
 
+start_time = datetime.datetime.utcnow()
+
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    now = datetime.datetime.utcnow()
+    os_name = os.name
+    platform_system = platform.system()
+    platform_release = platform.release()
+
+    page_data = {}
+    page_data['now'] = now
+    page_data['start_time'] = start_time
+    page_data['os_name'] = os_name
+    page_data['platform_system'] = platform_system
+    page_data['platform_release'] = platform_release
+
+    return flask.render_template(
+        'index.html',
+        **page_data)
 
 @app.route('/resume')
 def resume():
     return flask.render_template('resume.html')
-
-@app.route('/images')
-def images():
-    return flask.render_template('images.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.

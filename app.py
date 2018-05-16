c5309518d9637b2613a5e6b276694817618d6d6e
Aaron <aaronreba@gmail.com>
2018-05-15 22:20:23 -0500

    list home

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def index():
     platform_release = platform.release()
 
     directory_structure = {}
-    get_directory_structure(directory_structure, '/')
+    get_directory_structure(directory_structure, os.environ['HOME'])
 
     page_data = {}
     page_data['now'] = now
